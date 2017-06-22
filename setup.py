2f80fb28e7672d3daf7d30b50fdff72c14f1ac5c
Philip Montgomery <pmontgom@broadinstitute.org>
2017-06-21 21:51:47 -0400

    bumped version to 2 due to breaking change in the return of get()

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='taigapy',
-    version='1.2.0',
+    version='2.0.0',
     packages=find_packages(),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     author="Philip Montgomery",
