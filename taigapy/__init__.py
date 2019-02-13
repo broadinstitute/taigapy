7bdd7d3be93450e966e6038cf060bec52a8ba682
Rémi Marenco <remi.marenco@gmail.com>
2019-02-13 10:54:30 -0500

    Add bump version

--- taigapy/__init__.py
+++ taigapy/__init__.py
@@ -9,7 +9,7 @@ import sys
 
 from taigapy.UploadFile import UploadFile
 
-__version__ = "2.5.0"
+__version__ = "2.5.1"
 
 # global variable to allow people to globally override the location before initializing client
 # which is often useful in adhoc scripts being submitted onto the cluster.
