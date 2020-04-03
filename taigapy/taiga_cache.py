08e68350fcceee85bf8ade74e6edac74d75127f9
Yejia Chen <yejia@broadinstitute.org>
2020-04-03 09:10:01 -0400

    Reorganize cache; add stub tests

--- taigapy/taiga_cache.py
+++ taigapy/taiga_cache.py
@@ -1,19 +1,53 @@
 import os
+import shutil
 import sqlite3
+import pandas as pd
 
 from collections import namedtuple
-from typing import Optional, Tuple
+from typing import Mapping, Optional, Tuple
 
+from taigapy.types import DataFileType, DataFileFormat
+
+# Also needs to handle case where a virtual dataset contains only one file... ? Should that be an alias?
+
+# TODO: Redo this... Remove underlying_data_file and just lump that in with alias. I don't think cache needs
+#       to know whether something is an alias or underlying file, as long as it returns the right df
 DataFile = namedtuple(
-    "DataFile",
-    [
-        "full_taiga_id",
-        "underlying_taiga_id",
-        "raw_path",
-        "feather_path",
-        "datafile_type",
-    ],
+    "DataFile", ["full_taiga_id", "raw_path", "feather_path", "datafile_format",],
 )
+
+
+def _write_csv_to_feather(
+    csv_path: str,
+    feather_path: str,
+    datafile_format: DataFileFormat,
+    column_types: Optional[Mapping[str, str]] = None,
+    encoding: Optional[str] = None,
+) -> str:
+    if datafile_format == DataFileFormat.HDF5:
+        # https://github.com/pandas-dev/pandas/issues/25067
+        df = pd.read_csv(csv_path, index_col=0, encoding=encoding)
+        df = df.astype(float)
+
+        # Feather does not support indexes
+        df.reset_index().to_feather(feather_path)
+    else:
+        df = pd.read_csv(csv_path, dtype=column_types, encoding=encoding)
+        df.to_feather(feather_path)
+
+    return feather_path
+
+
+def _read_feather_to_df(path: str, datafile_format: DataFileFormat) -> pd.DataFrame:
+    """Reads and returns a Pandas DataFrame from a Feather file at `path`.
+
+    If `datafile_format` is "HDF5", we convert the first column to an index.
+    """
+    df = pd.read_feather(path)
+    if datafile_format == DataFileFormat.HDF5:
+        df.set_index(df.columns[0], inplace=True)
+        df.index.name = None
+    return df
 
 
 class TaigaCache:
@@ -36,10 +70,9 @@ class TaigaCache:
             """
             CREATE TABLE datafiles(
                 full_taiga_id TEXT NOT NULL PRIMARY KEY,
-                underlying_taiga_id TEXT,
-                raw_path TEXT,
+                raw_path TEXT NOT NULL,
                 feather_path TEXT,
-                datafile_type TEXT
+                datafile_format TEXT NOT NULL
             )
             """
         )
@@ -60,11 +93,47 @@ class TaigaCache:
         # Save (commit) the changes
         self.conn.commit()
 
-    def get_entry(self, queried_taiga_id: str) -> Optional[DataFile]:
+    def _get_path_and_make_directories(
+        self, full_taiga_id: str, extension: str,
+    ) -> str:
+        assert extension.startswith(".")
+        rel_file_path_without_extension = full_taiga_id.replace(".", "/")
+        file_path = os.path.join(
+            self.cache_dir, "{}.{}".format(rel_file_path_without_extension, extension),
+        )
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        return file_path
+
+    def _add_alias(self, queried_taiga_id: str, full_taiga_id: str):
         c = self.conn.cursor()
         c.execute(
             """
-            SELECT datafiles.full_taiga_id, underlying_taiga_id, raw_path, feather_path, datafile_type
+            SELECT
+                full_taiga_id
+            FROM
+                aliases
+            where
+                alias = ?
+            """,
+            (queried_taiga_id,),
+        )
+        if c.fetchone() is None:
+            c.execute(
+                """
+                INSERT INTO aliases
+                VALUES (?, ?)
+                """,
+                (queried_taiga_id, full_taiga_id),
+            )
+        c.close()
+        self.conn.commit()
+
+    def get_entry(self, queried_taiga_id: str) -> Optional[pd.DataFrame]:
+        raise NotImplementedError
+        c = self.conn.cursor()
+        c.execute(
+            """
+            SELECT datafiles.full_taiga_id, raw_path, feather_path, datafile_format
             FROM datafiles
             LEFT JOIN aliases
             ON
@@ -78,131 +147,190 @@ class TaigaCache:
 
         r = c.fetchone()
         c.close()
+
         if r is None:
             return None
 
         datafile = DataFile(*r)
-        if datafile.underlying_taiga_id is not None:
-            return self.get_entry(datafile.underlying_taiga_id)
-        return datafile
+        if datafile.feather_path is None:
+            return None
 
-    def get_entry_path_and_format(
-        self, queried_taiga_id: str
-    ) -> Tuple[Optional[str], Optional[str]]:
-        entry = self.get_entry(queried_taiga_id)
-        if entry is not None:
-            return entry.feather_path, entry.datafile_type
-        return None, None
-
-    def get_full_taiga_id(self, queried_taiga_id: str) -> Optional[str]:
-        entry = self.get_entry(queried_taiga_id)
-        if entry is not None:
-            return entry.full_taiga_id
-        return None
-
-    def get_path_and_make_directories(
-        self,
-        dataset_permaname: str,
-        dataset_version: str,
-        datafile_name: str,
-        extension: Optional[str] = None,
-    ) -> str:
-        file_path = os.path.join(
-            self.cache_dir,
-            dataset_permaname,
-            dataset_version,
-            "{}.{}".format(datafile_name, extension)
-            if extension is not None
-            else datafile_name,
-        )
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        return file_path
+        try:
+            return _read_feather_to_df(
+                datafile.feather_path, DataFileFormat(datafile.datafile_format)
+            )
+        except Exception as e:
+            self.remove_from_cache(queried_taiga_id)
+            raise Exception(
+                "Local file is corrupted. Deleting file from cache and trying again."
+            )
 
     def add_entry(
         self,
-        full_taiga_id: str,
         raw_path: Optional[str],
-        feather_path: Optional[str],
-        datafile_type: str,
+        queried_taiga_id: str,
+        full_taiga_id: str,
+        datafile_format: DataFileFormat,
+        column_types: Optional[Mapping[str, str]],
+        encoding: Optional[str],
     ):
+        """
+        TODO:
+        if already exists:
+            return
+        if datafile type is s3:
+            - generate path for feather
+            - create subdirectories if needed
+            - convert df to feather
+            - add to cache
+        else if datafile type is virtual:
+            (TaigaClient flow should have already added the real file?)
+            - 
+        """
+        self._add_alias(queried_taiga_id, full_taiga_id)
+
         c = self.conn.cursor()
-        entry = self.get_entry(full_taiga_id)
-        if entry is None:
+        c.execute(
+            """
+            SELECT full_taiga_id, raw_path, feather_path, datafile_format
+            FROM datafiles
+            WHERE
+                full_taiga_id = ?
+            """,
+            (full_taiga_id,),
+        )
+
+        r = c.fetchone()
+        datafile = DataFile(*r) if r is not None else None
+
+        if datafile is None and datafile_format == DataFileFormat.Raw:
+            raw_cache_path = self._get_path_and_make_directories(full_taiga_id, ".txt")
+            shutil.copy(raw_path, raw_cache_path)
             c.execute(
                 """
                 INSERT INTO datafiles
-                VALUES (?, ?, ?, ?, ?)
+                VALUES (?, ?, ?, ?)
                 """,
-                (full_taiga_id, None, raw_path, feather_path, datafile_type),
+                (full_taiga_id, raw_cache_path, None, datafile_format.value),
             )
-        elif entry.feather_path is None:
+            c.close()
+        elif datafile is None and datafile_format != DataFileFormat.Raw:
+            raw_cache_path = self._get_path_and_make_directories(full_taiga_id, ".csv",)
+            feather_path = self._get_path_and_make_directories(
+                full_taiga_id, ".feather"
+            )
+            shutil.copy(raw_path, raw_cache_path)
+            _write_csv_to_feather(
+                raw_cache_path, feather_path, datafile_format, column_types, encoding
+            )
+            c.execute(
+                """
+                INSERT INTO datafiles
+                VALUES (?, ?, ?, ?)
+                """,
+                (full_taiga_id, raw_cache_path, feather_path, datafile_format.value),
+            )
+        elif datafile is not None and datafile.feather_path is None:
+            assert datafile_format != DataFileFormat.Raw
+            if datafile_format == DataFileFormat.Columnar:
+                assert column_types is not None
+
+            feather_path = self._get_path_and_make_directories(
+                full_taiga_id, ".feather"
+            )
+            try:
+                _write_csv_to_feather(
+                    datafile.raw_path,
+                    feather_path,
+                    datafile_format,
+                    column_types,
+                    encoding,
+                )
+            except FileNotFoundError:
+                shutil.copy(raw_path, datafile.raw_path)
+                _write_csv_to_feather(
+                    datafile.raw_path,
+                    feather_path,
+                    datafile_format,
+                    column_types,
+                    encoding,
+                )
             c.execute(
                 """
                 UPDATE datafiles
-                SET feather_path = ?,
-                    datafile_type = ?
+                SET feather_path = ?
                 WHERE
                     full_taiga_id = ?
                 """,
-                (feather_path, datafile_type, full_taiga_id),
+                (feather_path, full_taiga_id),
             )
+
         c.close()
         self.conn.commit()
 
-    def add_alias(self, queried_taiga_id: str, full_taiga_id: str):
-        c = self.conn.cursor()
-        entry = self.get_entry(queried_taiga_id)
-        if entry is None:
-            c.execute(
-                """
-                INSERT INTO aliases
-                VALUES (?, ?)
-                """,
-                (queried_taiga_id, full_taiga_id),
-            )
-
-    def add_virtual_datafile(self, full_taiga_id: str, underlying_taiga_id: str):
-        c = self.conn.cursor()
-        entry = self.get_entry(full_taiga_id)
-        if entry is None:
-            c.execute(
-                """
-                INSERT INTO datafiles
-                VALUES (?, ?, ?, ?, ?)
-                """,
-                (full_taiga_id, underlying_taiga_id, None, None, None),
-            )
-        c.close()
-        self.conn.commit()
-
-    def update_datafile(
-        self,
-        full_taiga_id: str,
-        raw_path: Optional[str] = None,
-        feather_path: Optional[str] = None,
-        datafile_type: Optional[str] = None,
+    def add_raw_entry(
+        self, raw_path: str, full_taiga_id: str, datafile_format: DataFileFormat
     ):
-        entry = self.get_entry(full_taiga_id)
+        raise NotImplementedError
+
+    def remove_from_cache(self, queried_taiga_id: str):
+        raise NotImplementedError
+        datafile = self.get_entry(queried_taiga_id)
+        if datafile is None:
+            # TODO: add warning?
+            return
+
+        c = self.conn.cursor()
+
+        for p in [datafile.feather_path, datafile.raw_path]:
+            if os.path.exists(p):
+                os.remove(p)
+            else:
+                # TODO: add warning?
+                pass
+
+        c.execute(
+            """
+            DELETE FROM datafiles
+            WHERE full_taiga_id = '?'
+            """,
+            (datafile.full_taiga_id,),
+        )
+
+        c.execute(
+            """
+            DELETE FROM datafiles
+            WHERE full_taiga_id = '?'
+            """,
+            (datafile.full_taiga_id,),
+        )
+
+    def remove_all_from_cache(self, prefix: str):
+        raise NotImplementedError
+
+        assert prefix.endswith("/")
         c = self.conn.cursor()
 
         c.execute(
             """
-            UPDATE datafiles
-            SET raw_path = ?,
-                feather_path = ?,
-                datafile_type = ?
+            SELECT * FROM datafiles
             WHERE
-                full_taiga_id = ?
+                full_taiga_id LIKE '?%' or
+                underlyfing_file_id LIKE '?%'
             """,
-            (
-                raw_path if raw_path is not None else entry.raw_path,
-                feather_path if feather_path is not None else entry.raw_path,
-                datafile_type if datafile_type is not None else entry.raw_path,
-                full_taiga_id,
-            ),
+            (prefix, prefix),
         )
-        c.close()
-        self.conn.commit()
 
-    def remove_from_cache(self, queried_taiga_id: str):
+        datafiles_to_delete = c.fetchall()
+
+        c.execute(
+            """
+            SELECT * FROM aliases
+            WHERE
+                full_taiga_id LIKE '?%'
+            """,
+            (prefix, prefix),
+        )
+
+        aliases_to_delete = c.fetchall()
         raise NotImplementedError
--- taigapy/test_taiga_cache.py
+++ taigapy/test_taiga_cache.py
@@ -1,39 +1,158 @@
 import pdb
 
 import os
+import pandas as pd
 import py
 import pytest
+from typing import Mapping, Optional
+from unittest.mock import patch
 
 from taigapy.taiga_cache import TaigaCache, DataFile
-from taigapy import DataFileType, CACHE_FILE
 
-FULL_TAIGA_ID = "some-dataset.1/some-file"
-TAIGA_ID_ALIAS = "some-dataset.1"
-VIRTUAL_TAIGA_ID = "some-dataset.2/some-file"
-FEATHER_PATH = FULL_TAIGA_ID.replace(".", "/") + ".feather"
+# TODO: Fix once big bang version is finished 🙃
+from taigapy.__init_big_bang__ import CACHE_FILE
+from taigapy.types import DataFileType, DataFileFormat
+
+COLUMNAR_DATAFRAME = pd.DataFrame(
+    {"foo": [1, 2, 3], "bar": ["four", "five", "six"], "baz": [7, 8, 9]}
+)
+COLUMNAR_TYPES = {"foo": "float", "bar": "str", "baz": "float"}
+COLUMNAR_FULL_TAIGA_ID = "columnar-dataset.1/some-file"
+COLUMNAR_TAIGA_ID_ALIAS = "columnar-dataset.1"
+COLUMNAR_VIRTUAL_TAIGA_ID = "columnar-dataset.2/some-file"
+
+MATRIX_DATAFRAME = pd.DataFrame(
+    {"foo": [1, 2, 3], "bar": [4, 5, 6], "baz": [7, 8, 9]},
+    index=["wibble", "wobble", "wubble"],
+)
+MATRIX_FULL_TAIGA_ID = "matrix-dataset.1/some-file"
+MATRIX_TAIGA_ID_ALIAS = "matrix-dataset.1"
+MATRIX_VIRTUAL_TAIGA_ID = "matrix-dataset.2/some-file"
+
+
+@pytest.mark.parametrize(
+    "full_taiga_id,taiga_id_alias,virtual_taiga_id,datafile_format,column_types",
+    [
+        (
+            COLUMNAR_FULL_TAIGA_ID,
+            COLUMNAR_TAIGA_ID_ALIAS,
+            COLUMNAR_VIRTUAL_TAIGA_ID,
+            DataFileFormat.Columnar,
+            COLUMNAR_TYPES,
+        ),
+        (
+            MATRIX_FULL_TAIGA_ID,
+            MATRIX_TAIGA_ID_ALIAS,
+            MATRIX_VIRTUAL_TAIGA_ID,
+            DataFileFormat.HDF5,
+            None,
+        ),
+    ],
+)
+def test_add_entry(
+    populated_cache: TaigaCache,
+    full_taiga_id: str,
+    taiga_id_alias: str,
+    virtual_taiga_id: str,
+    datafile_format: DataFileFormat,
+    column_types: Optional[Mapping[str, str]],
+):
+    with patch(
+        "taigapy.taiga_cache._write_csv_to_feather"
+    ) as mock_write_csv_to_feather:
+        populated_cache.add_entry(
+            None, full_taiga_id, full_taiga_id, datafile_format, column_types, None
+        )
+        assert not mock_write_csv_to_feather.called
+
+        populated_cache.add_entry(
+            None, taiga_id_alias, full_taiga_id, datafile_format, column_types, None
+        )
+        assert not mock_write_csv_to_feather.called
+
+        populated_cache.add_entry(
+            None, virtual_taiga_id, full_taiga_id, datafile_format, column_types, None
+        )
+        assert not mock_write_csv_to_feather.called
 
 
 @pytest.fixture
 def populated_cache(tmpdir: py._path.local.LocalPath):
-
     cache = TaigaCache(str(tmpdir), str(tmpdir.join(CACHE_FILE)))
 
-    cache.add_entry(FULL_TAIGA_ID, None, FEATHER_PATH, DataFileType.HDF5.value)
-    cache.add_alias(TAIGA_ID_ALIAS, FULL_TAIGA_ID)
-    cache.add_virtual_datafile(VIRTUAL_TAIGA_ID, FULL_TAIGA_ID)
+    p = tmpdir.join("foobar.csv")
+    COLUMNAR_DATAFRAME.to_csv(p, index=False),
+
+    cache.add_entry(
+        str(p),
+        COLUMNAR_FULL_TAIGA_ID,
+        COLUMNAR_FULL_TAIGA_ID,
+        DataFileFormat.Columnar,
+        COLUMNAR_TYPES,
+        None,
+    )
+
+    MATRIX_DATAFRAME.astype(float).to_csv(p)
+    cache.add_entry(
+        str(p),
+        MATRIX_FULL_TAIGA_ID,
+        MATRIX_FULL_TAIGA_ID,
+        DataFileFormat.HDF5,
+        None,
+        None,
+    )
 
     return cache
 
 
-def test_get_entry(populated_cache: TaigaCache):
-    expected_datafile = DataFile(
-        FULL_TAIGA_ID, None, None, FEATHER_PATH, DataFileType.HDF5.value
-    )
-    real_file = populated_cache.get_entry(FULL_TAIGA_ID)
-    assert expected_datafile == real_file
+@pytest.mark.parametrize(
+    "expected_df,full_taiga_id,taiga_id_alias,virtual_taiga_id",
+    [
+        (
+            COLUMNAR_DATAFRAME,
+            COLUMNAR_FULL_TAIGA_ID,
+            COLUMNAR_TAIGA_ID_ALIAS,
+            COLUMNAR_VIRTUAL_TAIGA_ID,
+        ),
+        (
+            MATRIX_DATAFRAME,
+            MATRIX_FULL_TAIGA_ID,
+            MATRIX_TAIGA_ID_ALIAS,
+            MATRIX_VIRTUAL_TAIGA_ID,
+        ),
+    ],
+)
+def test_get_entry(
+    populated_cache: TaigaCache,
+    expected_df: pd.DataFrame,
+    full_taiga_id: str,
+    taiga_id_alias: str,
+    virtual_taiga_id: str,
+):
+    """
+    TODO:
+    - get full_taiga_id should return the df
+    - get virtual taiga id should not return the df until the link has been added
+        - then add virtual taiga id (need to figure out parameters)
+        - get virtual taiga id should return df
+        - same for alias
+    - removing the actual file should remove the entry just for the actual taiga id,
+      but not the aliases/virtual taiga id
+    """
+    assert populated_cache.get_entry(full_taiga_id).equals(expected_df)
+    # TODO
 
-    aliased_file = populated_cache.get_entry(TAIGA_ID_ALIAS)
-    assert expected_datafile == real_file
 
-    virtual_file = populated_cache.get_entry(VIRTUAL_TAIGA_ID)
-    assert expected_datafile == real_file
+def test_add_raw_entry(tmpdir):
+    cache = TaigaCache(str(tmpdir), str(tmpdir.join(CACHE_FILE)))
+
+    p = tmpdir.join("foobar.txt")
+    cache.add_raw_entry
+
+
+def test_remove_from_cache(populated_cache: TaigaCache):
+    populated_cache.remove_from_cache
+
+
+def test_remove_all_from_cache(populated_cache: TaigaCache):
+    populated_cache.remove_all_from_cache
