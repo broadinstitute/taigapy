40ea382a2df01e755d0789532bee465b2a78f31c
Philip Montgomery <pmontgom@broadinstitute.org>
2022-03-23 14:43:44 -0400

    Moved TaigaClient out of taigapy/__init__.py

---
+++ taigapy/consts.py
@@ -0,0 +1,6 @@
+DEFAULT_TAIGA_URL = "https://cds.team/taiga"
+
+# global variable to allow people to globally override the location before initializing client
+# which is often useful in adhoc scripts being submitted onto the cluster.
+DEFAULT_CACHE_DIR = "~/.taiga"
+CACHE_FILE = ".cache.db"
