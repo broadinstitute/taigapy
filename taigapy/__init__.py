5419bb8d78439cce109908adbd5690b54c5da563
Philip Montgomery <pmontgom@broadinstitute.org>
2023-08-14 17:02:41 -0400

    added default_tc3 because Bella asked nicely.

--- taigapy/__init__.py
+++ taigapy/__init__.py
@@ -18,3 +18,11 @@ def create_taiga_client_v3(*args, **kwargs):
     tc = TaigaClient(*args, **kwargs)
     tc._set_token_and_initialized_api()
     return ClientV3(tc.cache_dir, tc.api)
+
+try:
+    default_tc3 = create_taiga_client_v3()
+except Exception as e:
+    print("default_tc could not be set for this reason: {}".format(e))
+    print(
+        "You can import create_taiga_client_v3 and add your custom options if you would want to customize it to your settings"
+    )
