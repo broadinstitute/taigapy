962dfb062790334e8685978c83a1ce85497c052d
Jessica Cheng <jcheng@broadinstitute.org>
2024-04-11 09:52:36 -0400

    test: Test pre-commit-merge hook

--- taigapy/__init__.py
+++ taigapy/__init__.py
@@ -23,6 +23,7 @@ def create_taiga_client_v3(*args, **kwargs):
     tc._set_token_and_initialized_api()
     return ClientV3(tc.cache_dir, tc.api)
 
+
 try:
     default_tc3 = create_taiga_client_v3()
 except Exception as e:
@@ -30,3 +31,5 @@ except Exception as e:
     print(
         "You can import create_taiga_client_v3 and add your custom options if you would want to customize it to your settings"
     )
+
+## Test pre-merge-commit hook
